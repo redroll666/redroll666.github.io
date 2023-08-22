4b001389f8c28f7dbc7e0ff189731ac845360260
AinLume <89462838+redroll666@users.noreply.github.com>
2023-08-22 21:05:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 let tg = window.Telegram.WebApp
 
-// получаем DOM-элемент слайдера
-const sliderElem = document.querySelector('.slider');
-// получаем его экземпляр класса ItcSlider
-const slider = ItcSlider.getInstance(sliderElem);
+let slideIndex = 1;
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
 
-let button_prev = document.querySelector(".itc-slider-btn itc-slider-btn-prev").OnClick = function() {
-    slider.prev();
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
 }
-let button_next = document.querySelector(".itc-slider-btn itc-slider-btn-next").OnClick = function() {
-    slider.next();
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function prevSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
+    let slides = document.getElementsByClassName("item");
+
+    /* Проверяем количество слайдов: */
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    /* Проходим по каждому слайду в цикле for: */
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    /* Делаем элемент блочным: */
+    slides[slideIndex - 1].style.display = "block";
 }
