11d224f6ce8600b1daf9964330a4a9e6831a3ddc
AinLume <89462838+redroll666@users.noreply.github.com>
2023-08-25 12:46:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let tg = window.Telegram.WebApp;
 tg.expand()
 
 let reg = document.getElementById("reg")
+
 reg.addEventListener("click", () => {
+    let name = document.getElementById("name").value;
+    let description = document.getElementById("description").value;
+    let photo = document.getElementById("avatar").value;
+    
+    let data = {
+        name: name,
+        description: description,
+        photo: photo
+    }
+    
+    tg.sendData(JSON.stringify(data));
+
     tg.close();
 });
